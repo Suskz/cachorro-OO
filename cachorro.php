dc85d212c6e47e9bcf1268c8bae51ebc797bf104
Suskz <62948759+Suskz@users.noreply.github.com>
2020-05-21 11:24:23 -0300

    Add files via upload

---
+++ cachorro.php
@@ -0,0 +1,20 @@
+<?php
+class cachorro{
+    public $raca;
+
+    public function __construct($raca){
+      $this->raca = $raca;
+      echo("Cachorro " .$this->raca. " nasceu \n ");
+    }
+
+    public function __destruct() {
+      echo("O " .$this->raca ." Morreu \n");
+    }
+
+    public function latir(){
+      echo(" AUAU \n ");
+    }
+}
+$bob = new cachorro("puddle");
+$bob ->latir();
+?>
---
+++ main.php
@@ -0,0 +1,5 @@
+<?php
+require_once'cachorro.php';
+$bob = new cachorro("puddle");
+$bob->latir();
+?>
